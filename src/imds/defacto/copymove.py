7e505504ca1d9a497c36030a5e0ab5c4195e9169
Spencer Cain <cainspencerm@protonmail.com>
2025-07-02 09:48:37 -0400

    feat: implement the _BaseDataset image_files property for the Defacto CopyMove dataset

--- src/imds/defacto/copymove.py
+++ src/imds/defacto/copymove.py
@@ -1,5 +1,5 @@
 import os
-from typing import Tuple
+from typing import List, Tuple
 
 import numpy as np
 
@@ -95,19 +95,19 @@ class CopyMove(_BaseDataset):
 
         # Note that the order of the output files is aligned with the input files.
         if split == "train":
-            self.image_files = image_files[: split_size * 8]
+            self._image_files = image_files[: split_size * 8]
 
         elif split == "valid":
-            self.image_files = image_files[split_size * 8 : split_size * 9]
+            self._image_files = image_files[split_size * 8 : split_size * 9]
 
         elif split == "test":
-            self.image_files = image_files[split_size * 9 :]
+            self._image_files = image_files[split_size * 9 :]
 
         elif split == "benchmark":
-            self.image_files = image_files[:1000]
+            self._image_files = image_files[:1000]
 
         elif split == "full":
-            self.image_files = image_files
+            self._image_files = image_files
 
         else:
             raise ValueError(f"Unknown split: {split}")
@@ -116,7 +116,7 @@ class CopyMove(_BaseDataset):
         mask_dir = os.path.join(data_dir, "copymove_annotations", "probe_mask")
 
         self.mask_files = []
-        for f in self.image_files:
+        for f in self._image_files:
             f = f.split("/")[-1]
             mask_file = os.path.abspath(os.path.join(mask_dir, f))
             if not os.path.exists(mask_file) and mask_file[-3:] == "jpg":
@@ -125,3 +125,8 @@ class CopyMove(_BaseDataset):
                 self.mask_files.append(mask_file.replace(".tif", ".jpg"))
             else:
                 self.mask_files.append(mask_file)
+
+    @property
+    def image_files(self) -> List[str]:
+        """Returns the list of image files in the dataset."""
+        return self._image_files
